de417fcf27306aa90b2ebd2a8e30be59dd99f1f7
Kuyee Chang <kyec1118@gmail.com>
2024-11-27 01:42:39 +0800

    replace add hero form with formGroup in hero component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroService } from '../hero.service';
 import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, ReactiveFormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
+  newHeroForm = new FormGroup({
+    name: new FormControl<string>(''),
+  });
+
   constructor(private heroService: HeroService) {}
   ngOnInit(): void {
     this.getHeroes();
@@ -22,14 +27,15 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
-  add(name: string): void {
-    name.trim();
+  add(): void {
+    const name = this.newHeroForm.controls.name.value?.trim();
     if (!name) {
       return;
     }
     this.heroService
       .addHero({ name } as Hero)
       .subscribe((hero) => this.heroes.push(hero));
+    this.newHeroForm.reset();
   }
 
   delete(hero: Hero): void {
